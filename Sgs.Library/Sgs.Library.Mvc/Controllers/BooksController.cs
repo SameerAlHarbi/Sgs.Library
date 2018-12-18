66ea47d6fd7bb41fa5b221fc2a4ce8ec9262ff4e
Osama Najjar <Najjar.OM@sgs.org.sa>
2018-12-18 16:39:42 +0300

    Layout v0.1 18-12-2018 add Navbar , Footer

--- Sgs.Library/Sgs.Library.Mvc/Controllers/BooksController.cs
+++ Sgs.Library/Sgs.Library.Mvc/Controllers/BooksController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Sgs.Library.Model;
 using Sgs.Library.BusinessLogic;
-using Sgs.Library.Mvc.Services;
+using System.Threading.Tasks;
 
 namespace Sgs.Library.Mvc.Controllers
 {
