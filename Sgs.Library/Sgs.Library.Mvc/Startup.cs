503a6991d990380bdfabadc07806d13b98ffa6bc
Sameer Al Harbi <smrhrbi@gmail.com>
2018-12-23 19:34:44 +0300

    startup work

--- Sgs.Library/Sgs.Library.Mvc/Startup.cs
+++ Sgs.Library/Sgs.Library.Mvc/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
